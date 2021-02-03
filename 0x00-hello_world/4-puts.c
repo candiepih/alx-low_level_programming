80166ba6feaa24f1441b4da801c089d108d5ad7e
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-02-03 17:54:31 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts(Mystr);
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
 }
