30574367056862c051cea1cc149c237c20fd3d29
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-02-03 21:46:13 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	write(STDERR_FILENO,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 55);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
 	return (1);
 }
