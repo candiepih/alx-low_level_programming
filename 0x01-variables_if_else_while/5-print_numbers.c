0377a98ba10889e9d09ff0fecc455e0e827e15de
root <mutheeal.am@gmai.com>
2021-02-04 15:07:07 +0000

    added task file 4-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing base 10 numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
