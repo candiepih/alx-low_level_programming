692a07149ef8a8dbe27ad3e287bfb0c616ef8253
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-02-09 23:27:53 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (i / 10 != 0)
 			putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i != 99)
+		if (i != 9)
 			putchar(',');
 			putchar(' ');
 	}
