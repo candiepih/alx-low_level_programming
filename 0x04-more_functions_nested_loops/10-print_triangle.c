41b70be65040fee31ba92b068fabe01970be0fe5
candiepih <mutheeal.am@gmail.com>
2021-02-11 15:23:45 +0000

    added task file 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - drawing triangle depending on size
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int spaces, i, s, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		spaces = size - i;
+		for (s = 0; s < spaces; s++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < i; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if(size <= 0)
+	{
+		_putchar('\n');
+	}
+}
