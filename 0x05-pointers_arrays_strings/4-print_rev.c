4698629dd1f7330e34bf37fff3fe51da53811c24
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-02-16 18:37:48 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,17 @@
 
 void print_rev(char *s)
 {
-	int length = 0, i;
+	int l = 0, i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		length++;
+		l++;
 	}
 	
 	while (length >= 0)
 	{
-		_putchar(s[length]);
-		length--;
+		_putchar(s[l]);
+		l--;
 	}
 	_putchar('\n');
 }
