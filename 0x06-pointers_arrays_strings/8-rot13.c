490a0e670933229f3d401c1a88ab138f58871818
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-02-19 00:39:53 +0300

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,22 +7,21 @@
 char *rot13(char *s)
 {
 	int i;
-	/**
-	 * int j;
-	 *char alphUpper[14] = {
-	 *	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
-	 * };
-	 * char alphLower[14] = {
-	 *	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-	 * };
-	*/
+	int j;
+	char alphUpper[14] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+	};
+	char alphLower[14] = {
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] > 'A' && s[i] < 'N') || (s[i] > 'a' && s[i] < 'n'))
-			s[i] += 13;
-		else if ((s[i] > 'M' && s[i] < 'Z') || (s[i] > 'm' && s[i] < 'z'))
-			s[i] -= 13;
+		for(j = 0; alphUpper[j] != '\0'; j++){
+			if(s[i] == alphLower[j]){
+				s[i] = alphUpper[j];
+			}
+		}
 	}
 
 	return (s);
