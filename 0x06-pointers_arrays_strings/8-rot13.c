d498c3abe0774bff6d52dd63ccf2fc1bdbcf0b07
candiepih <mutheeal.am@gmail.com>
2021-02-18 21:32:43 +0000

    shortened the rot13 function

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,23 +6,23 @@
 
 char *rot13(char *s)
 {
-	int i, j;
-	char alphUpper[14] = {
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
-	};
-	char alphLower[14] = {
-		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-	};
+	int i;
+	/**
+	 * int j;
+	 *char alphUpper[14] = {
+	 *	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+	 * };
+	 * char alphLower[14] = {
+	 *	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	 * };
+	*/
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; alphUpper[j] != '\0'; j++)
-		{
-			if (s[i] == alphLower[j])
-			{
-				s[i] = alphUpper[j];
-			}
-		}
+		if ((s[i] > 'A' && s[i] < 'N') || (s[i] > 'a' && s[i] < 'n'))
+			s[i] += 13;
+		else if ((s[i] > 'M' && s[i] < 'Z') || (s[i] > 'm' && s[i] < 'z'))
+			s[i] -= 13;
 	}
 
 	return (s);
