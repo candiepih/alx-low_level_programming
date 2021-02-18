ef87de4b7e2900886415dc264fcc16fee90a576a
candiepih <mutheeal.am@gmail.com>
2021-02-18 20:47:45 +0000

    formatted according to betty linter

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,18 +4,22 @@
  * Return: pointer to the resulting string
  */
 
-char *leet(char *s){
+char *leet(char *s)
+{
 	int i, j;
 	char alphLower[6] = {'a', 'e', 'o', 't', 'l'};
 	char alphUpper[6] = {'A', 'E', 'O', 'T', 'L'};
 	char val[6] = {'4', '3', '0', '7', '1'};
 
-	for(i = 0; s[i] != '\0'; i++){
-		for(j = 0; alphLower[j] != '\0'; j++){
-			if((s[i] == alphLower[j]) || (s[i] == alphUpper[j])){
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphLower[j] != '\0'; j++)
+		{
+			if ((s[i] == alphLower[j]) || (s[i] == alphUpper[j]))
+			{
 				s[i] = val[j];
 			}
 		}
 	}
-	return s;
+	return (s);
 }
