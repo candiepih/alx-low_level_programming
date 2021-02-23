97958b7051710a048316b44efd0e2e6bc2abf20b
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-02-23 17:27:52 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,10 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while(*s != '\0')
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if(*s == c)
+			return (s);
 	}
 
 	return (0);
