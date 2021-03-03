9f760f323e7cd15a8b6aebd18382dc555ac5ab91
candiepih <mutheeal.am@gmail.com>
2021-03-03 14:10:48 +0000

    added advanced task file

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+
+/**
+ * extractWord - extracts a word starting from a particular
+ * index to the index a space is encountered
+ * @s: string to extract the word
+ * @start: index to start extracting string from
+ * Return: pointer the string
+ */
+
+char *extractWord(char *s, int start)
+{
+	int i = start;
+	int j = 0;
+	char *newString;
+
+	while (s[i] != ' ' && s[i] != '\0')
+		i++;
+
+	newString = malloc(sizeof(char) * i);
+
+	while (s[start] != ' ' && s[start] != '\0')
+	{
+		newString[j] = s[start];
+		start++;
+		j++;
+	}
+	newString[j] = '\0';
+
+	return (newString);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split words from
+ * Return: pointer to array of strings
+ */
+char **strtow(char *str)
+{
+	char **arr;
+	int i, j = 0;
+	int totalWords = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			if (str[i + 1] == ' ')
+				totalWords++;
+
+		}
+	}
+
+	arr = malloc(sizeof(char *) * (totalWords + 1));
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			if (str[i - 1] == ' ')
+			{
+				arr[j] = extractWord(str, i);
+				j++;
+			}
+		}
+	}
+
+	return (arr);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,5 +8,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 void _str_concat(char *s1, char *s2);
-
+char **strtow(char *str);
+char *extractWord(char *s, int start);
 #endif
