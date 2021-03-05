ea1355e56062221a71354d3160c98ffd17b8c962
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-05 12:36:27 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -64,7 +64,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	newString = malloc(sizeof(*newString) * fullLength);
 	for (i = 0; i < len1; i++)
 		newString[i] = s1[i];
-  
+
 	for (j = 0; j < (int)n; i++, j++)
 		newString[i] = s2[j];
 
