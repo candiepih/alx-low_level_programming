4724dc6834a03f4efbbdae16cac7dcd86cb1a20c
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-05 16:25:22 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		if (newlocation)
-			return newlocation;
+			return (newlocation);
 	}
 
 	if (!newlocation)
