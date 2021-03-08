b24057e4fd2ec3e94883eb77b83606886716086c
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-08 23:53:58 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#define IS_NULL(x) (x == NULL ? "(nil)" : x)
+#define IS_NULL(x) ((x == NULL) ? ("(nil)") : (x))
 
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
