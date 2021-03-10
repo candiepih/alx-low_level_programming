1cbf45a22d098fb6efa7ad259ba5d819f9b77245
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-10 18:20:37 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif HOLBERTON_H
