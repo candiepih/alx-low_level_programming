856a8477dd67c966c037a495e9cc9ed2236bf7f3
candiepih <mutheeal.am@gmail.com>
2021-03-12 14:39:51 +0000

    formatted file according to betty linter

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
  * print_all - prints arguments provided depending on format
  * @format: constant pointer to a string
@@ -6,34 +9,34 @@
 
 void print_all(const char * const format, ...)
 {
-  int i = 0;
-  va_list list;
-  char *s;
+	int i = 0;
+	va_list list;
+	char *s;
 
-  va_start(list, format);
-  while (format && format[i])
-  {
-    switch (format[i++])
-    {
-      case 'c':
-        printf("%c", va_arg(list, int));
-        break;
-      case 'i':
-        printf("%d", va_arg(list, int));
-        break;
-      case 'f':
-        printf("%f", (float)va_arg(list, double));
-        break;
-      case 's':
-        s = va_arg(list, char *);
-        printf("%s", (s ? s : "(nil)"));
-        break;
-      default:
-        continue;
-    }
-    if (format[i])
-      printf(", ");
-  }
-  printf("\n");
-  va_end(list);
+	va_start(list, format);
+	while (format && format[i])
+	{
+		switch (format[i++])
+		{
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(list, double));
+			break;
+		case 's':
+			s = va_arg(list, char *);
+			printf("%s", (s ? s : "(nil)"));
+			break;
+		default:
+			continue;
+		}
+		if (format[i])
+			printf(", ");
+	}
+	printf("\n");
+	va_end(list);
 }
