c83573589a1a93ff237c19274e5ef9bb961cdcb2
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-12 17:26:19 +0300

    Create 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,33 @@
+void print_all(const char * const format, ...)
+{
+  int i = 0;
+  va_list list;
+  char *s;
+
+  va_start(list, format);
+  while (format && format[i])
+  {
+    switch (format[i++])
+    {
+      case 'c':
+        printf("%c", va_arg(list, int));
+        break;
+      case 'i':
+        printf("%d", va_arg(list, int));
+        break;
+      case 'f':
+        printf("%f", (float)va_arg(list, double));
+        break;
+      case 's':
+        s = va_arg(list, char *);
+        printf("%s", (s ? s : "(nil)"));
+        break;
+      default:
+        continue;
+    }
+    if (format[i])
+      printf(", ");
+  }
+  printf("\n");
+  va_end(list);
+}
