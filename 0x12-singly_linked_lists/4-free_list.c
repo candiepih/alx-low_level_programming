2e6552f8c2e022f2e0ff57cafd05bc6d48260eb8
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-18 23:04:01 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,12 @@
 
 void free_list(list_t *head)
 {
-	if (head->next)
-		free_list(head->next);
-	if (head->str)
-		free(head->str);
 	if (head)
+	{
+		if (head->next)
+			free_list(head->next);
+		if (head->str)
+			free(head->str);
 		free(head);
+	}
 }
