17461c0a85233b7b4f009ec6779886975912b620
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-18 23:28:47 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,13 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
+		if (h->str)
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
 		i++;
 		h = h->next;
 	}
-
 	return (i);
 }
