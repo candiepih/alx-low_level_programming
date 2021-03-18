85eeaf1fd3b9b1e5955033e628e29bfd076aeb23
candiepih <mutheeal.am@gmail.com>
2021-03-18 18:30:56 +0000

    formatted according to betty linter

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -36,14 +36,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = _strlen((char *)str);
 	new_node->next = NULL;
 
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 	}
 	else
 	{
 
-		while(last_node->next != NULL)
+		while (last_node->next != NULL)
 			last_node = last_node->next;
 
 		last_node->next = new_node;
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,7 +8,7 @@
 
 void free_list(list_t *head)
 {
-	if(head->next)
+	if (head->next)
 		free_list(head->next);
 	free(head);
 }
