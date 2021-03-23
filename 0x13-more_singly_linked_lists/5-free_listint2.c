f9056dbdcf5f4998297c2266931114f269003ed7
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-23 15:54:46 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,14 @@
 
 void free_listint2(listint_t **head)
 {
-	if (*head && ((*head)->next != NULL))
-		free_listint2((&(*head)->next));
-	free(*head);
+	listint_t tmp = *head;
+	listint_t current_node;
+
+	while (tmp)
+	{
+		current_node = tmp->next;
+		free(tmp);
+		tmp = current_node;
+	}
 	*head = NULL;
 }
