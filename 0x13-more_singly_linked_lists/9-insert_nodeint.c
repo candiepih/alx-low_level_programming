919c85b6a06352c89cdcfb595942bb29fdef65b9
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-23 18:30:50 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,6 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = *head;
 			*head = new_node;
+			return (*head);
 		}
 		else if (i == ((int)idx - 1))
 		{
