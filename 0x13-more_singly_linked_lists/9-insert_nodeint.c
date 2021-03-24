b32f6a94ea3555c264966e24728a6f16ee9ba7bc
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-24 12:57:12 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,35 +14,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	int i = 0;
 
-	if (!head)
-		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-	tmp = *head;
-
-	while (tmp)
+	if (head)
 	{
-		if (((int)idx == 0) && (i == 0))
-		{
-			new_node->next = *head;
-			*head = new_node;
-		}
-		else if (i == ((int)idx - 1))
-		{
-			new_node->next = tmp->next;
-			tmp->next = new_node;
-		}
-		tmp = tmp->next;
-		i++;
-	}
+		new_node = malloc(sizeof(listint_t));
+		if (!new_node)
+			return (NULL);
 
-	if (((int)idx > i) || ((int)idx < 0))
-		return (NULL);
+		new_node->n = n;
+		tmp = *head;
+
+		while (tmp)
+		{
+			if (((int)idx == 0) && (i == 0))
+			{
+				new_node->next = *head;
+				*head = new_node;
+			}
+			else if (i == ((int)idx - 1))
+			{
+				new_node->next = tmp->next;
+				tmp->next = new_node;
+			}
+			tmp = tmp->next;
+			i++;
+		}
+
+		if (((int)idx > i) || ((int)idx < 0))
+			return (NULL);
+	}
 
 	return (new_node);
 }
