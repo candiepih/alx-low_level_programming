64b3417f8f36681f9f778466cc55dbba4a5f77a3
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-24 13:56:49 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,33 +13,32 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node_tmp;
 	int i = 0;
 
-	if (!head)
-		return (NULL);
-
-	tmp = *head;
-
-	while (tmp)
+	if (head)
 	{
-		if (index == 0 && i == 0)
+		tmp = *head;
+
+		while (tmp)
 		{
-			node_tmp = (*head)->next;
-			free(*head);
-			*head = node_tmp;
-			return (1);
+			if (index == 0 && i == 0)
+			{
+				node_tmp = (*head)->next;
+				free(*head);
+				*head = node_tmp;
+				return (1);
+			}
+			else if (i == ((int)index - 1))
+			{
+				node_tmp = tmp->next;
+				tmp->next = node_tmp->next;
+				free(node_tmp);
+				return (1);
+			}
+			tmp = tmp->next;
+			i++;
 		}
-		else if (i == ((int)index - 1))
-		{
-			node_tmp = tmp->next;
-			tmp->next = node_tmp->next;
-			free(node_tmp);
-			return (1);
-		}
-		tmp = tmp->next;
-		i++;
+		if (((int)index > i) || ((int)index < 0))
+			return (-1);
 	}
 
-	if (((int)index > i) || ((int)index < 0))
-		return (-1);
-
-	return (1);
+	return (-1);
 }
