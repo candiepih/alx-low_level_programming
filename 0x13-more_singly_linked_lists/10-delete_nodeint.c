3ec98974377d41b5b4c26ec70ce4b51e97edd0ab
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-24 13:52:29 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	int i = 0;
 
 	if (!head)
-		return (0);
+		return (-1);
 
 	tmp = *head;
 
@@ -38,7 +38,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	if ((int)index > i || (int)index < 0)
+	if (((int)index > i) || ((int)index < 0))
 		return (-1);
 
 	return (1);
