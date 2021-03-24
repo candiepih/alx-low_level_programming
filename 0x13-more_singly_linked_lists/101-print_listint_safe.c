5f865533b366f2385915c71efdf895814c408dfd
candiepih <mutheeal.am@gmail.com>
2021-03-24 12:56:21 +0000

    formatted file in accordance to betty linter

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,31 +4,45 @@
  * print_listint_safe - prints a listint_t linked list.
  * @head: starting point of a listint_t linked list
  * Description: detects a cycle in the listint-t linked list by using
- * floyd's cycle algorithm 
+ * floyd's cycle algorithm
  * Return: number of nodes
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-  int i = 0;
-  listint_t *slow = (listint_t *)head;
-  listint_t *fast = (listint_t *)head;
+	int i = 0;
+	listint_t *slow;
+	listint_t *fast;
 
-    while(slow) {
-        i++;
-        printf("[%p] %d\n", slow, slow->n);
-        slow = slow->next;
-        if (fast && fast->next){
-          fast = fast->next->next;
-          if (fast == slow){
-            printf("[%p] %d\n", slow, slow->n);
-            slow = slow->next;
-            printf("[%p] %d\n", slow, slow->n);
-            printf("-> [%p] %d\n", slow->next, slow->next->n);
-            i += 2;
-            exit(98);
-          }
-        }
-    }
+	if (!head)
+		exit(98);
+
+	slow = (listint_t *)head;
+	fast = (listint_t *)head;
+
+	while (slow)
+	{
+		i++;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		if (fast && fast->next)
+		{
+			fast = fast->next->next;
+			if (fast == slow)
+			{
+				printf("[%p] %d\n", (void *)slow, slow->n);
+				slow = slow->next;
+				fast = slow;
+				if (slow == fast)
+				{
+					printf("[%p] %d\n", (void *)slow, slow->n);
+					printf("-> [%p] %d\n", (void *)slow->next, slow->next->n);
+					i += 2;
+					break;
+				}
+			}
+		}
+	}
+
 	return (i);
 }
