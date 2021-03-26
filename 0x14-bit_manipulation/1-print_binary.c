7c8e6d22390ecccbf26a39cfbc1bdf3f349241cb
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-26 22:33:51 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,17 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, temp = n, length = 0;
-
-	if (n == 0)
-		_putchar(0 + '0');
-
-	while (temp)
-	{
-		length++;
-		temp >>= 1;
-	}
-
-	for (i = 0; i < length; i++)
-		_putchar(((n >> i) & 0x1) + '0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar(n & 1);
 }
