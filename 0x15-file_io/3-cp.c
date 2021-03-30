02e874380fb52f3e8ed427e711a1a09b6f19f92a
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-30 16:22:01 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,45 @@
 #include "holberton.h"
 
 /**
+ * main - Entry point.
+ * @argc: arguments count
+ * @argv: arguments vector, an array of string arguments supplied
+ * Description: handles arguments and makes necessary calls for
+ * file copying handling
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int fd, fd2;
+	char buffer[1024];
+
+	if (argc !=  3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	buffer_handling(fd, fd2, buffer, argv[1], argv[2]);
+
+	return (0);
+}
+
+/**
  * buffer_handling - copies a buffer string to file 2
  * @fd: file 1 descriptor
  * @fd2: file 2 descriptor
@@ -40,43 +79,4 @@ void buffer_handling(int fd, int fd2, char *buffer, char *f1, char *f2)
 		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", fd2);
 		exit(100);
 	}
-}
-
-/**
- * main - Entry point.
- * @argc: arguments count
- * @argv: arguments vector, an array of string arguments supplied
- * Description: handles arguments and makes necessary calls for
- * file copying handling
- * Return: Always 0.
- */
-
-int main(int argc, char **argv)
-{
-	int fd, fd2;
-	char buffer[1024];
-
-	if (argc !=  3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (fd2 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	buffer_handling(fd, fd2, buffer, argv[1], argv[2]);
-
-	return (0);
 }
