491d56f8c1ef36f34e8c8980899f3ce3f8620c75
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-03-30 16:15:21 +0300

    Update holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -10,5 +10,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void buffer_handling(int fd, int fd2, char *buffer, char *f1, char *f2);
 
 #endif
