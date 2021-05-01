7f376062cdfe45b727e92b7853b5c2e909860fb1
candiepih <mutheeal.am@gmail.com>
2021-05-01 11:40:47 +0000

    formatted file in accordance to betty linter

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,9 +30,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (index == idx)
 		{
 			new_node->n = n;
-                        new_node->next = current_node;
-                        new_node->prev = current_node->prev;
-                        current_node->prev->next = new_node;
+			new_node->next = current_node;
+			new_node->prev = current_node->prev;
+			current_node->prev->next = new_node;
 			return (new_node);
 		}
 		current_node = current_node->next;
