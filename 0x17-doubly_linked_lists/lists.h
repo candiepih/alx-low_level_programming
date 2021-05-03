83fbe635fed4b82b55bdbc1f2b772cdd71232f74
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-03 09:55:30 +0300

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -30,5 +30,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int remove_node_start(dlistint_t **head, dlistint_t *current_node);
 
 #endif
