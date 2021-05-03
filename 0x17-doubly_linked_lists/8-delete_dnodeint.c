38ffe596f0eebb61cf2660354a6b93fa61b80f6d
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-03 09:54:54 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+ * remove_node_start - removes a node at start of list
+ * @head: beginning of dlistint_t linked list
+ * @current_node: the node at index 0
+ * Return: (1) on Success (-1) of failure
+ */
+
+int remove_node_start(dlistint_t **head, dlistint_t *current_node)
+{
+	dlistint_t *tmp;
+	if (!current_node->next)
+	{
+		free(current_node);
+		*head = NULL;
+		return (1);
+	}
+	else
+	{
+		tmp = current_node->next;
+		free(current_node);
+		tmp->prev = NULL;
+		*head = tmp;
+		return (1);
+	}
+	return (-1);
+}
+/**
  * delete_dnodeint_at_index - deletes a node at a given index
  * @head: beginning of our dlistint_t linked list
  * @index: position of the node to delete
@@ -18,19 +44,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	current_node = *head;
 	if ((int)index == 0)
-	{
-		if (!current_node->next)
-		{
-			free(current_node);
-			*head = NULL;
-			return (1);
-		}
-		tmp = current_node->next;
-		free(current_node);
-		tmp->prev = NULL;
-		*head = tmp;
-		return (1);
-	}
+		return (remove_node_start(head, current_node));
 
 	while (current_node)
 	{
