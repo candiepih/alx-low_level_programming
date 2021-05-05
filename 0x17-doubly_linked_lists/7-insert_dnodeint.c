87fcef458119ed9dd02be094c857ea54a621e53f
candiepih <mutheeal.am@gmail.com>
2021-05-05 07:06:43 +0000

    formatted file in accordance to betty linting style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,21 +17,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
-
 	tmp = *h;
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
-
 	if ((int)idx == 0)
 	{
 		free(new_node);
 		return (add_dnodeint(h, n));
 	}
-
 	while (tmp)
 	{
 		if (i == ((int)idx - 1))
@@ -49,12 +45,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
-
 	if (((int)idx > i) || ((int)idx < 0))
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	return (new_node);
 }
