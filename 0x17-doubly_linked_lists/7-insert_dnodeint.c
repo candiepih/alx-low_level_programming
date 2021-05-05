36994d522cca2de94dc6745150dd3ab2b6a330d6
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-05 10:00:30 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,39 +11,50 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp;
 	dlistint_t *new_node;
-	dlistint_t *current_node;
-	unsigned int current_index = 0;
+	int i = 0;
 
-	if (!h || ((int)n < 0))
+	if (!h)
 		return (NULL);
-	if ((int)idx == 0)
-		return (add_dnodeint(h, n));
 
+	tmp = *h;
 	new_node = malloc(sizeof(dlistint_t));
+
 	if (!new_node)
 		return (NULL);
 
-	current_node = *h;
-	while (current_node)
-	{
-		if (current_index == idx)
-		{
-			new_node->n = n;
-			new_node->next = current_node;
-			new_node->prev = current_node->prev;
-			current_node->prev->next = new_node;
-			return (new_node);
-		}
-		current_node = current_node->next;
-		current_index++;
-	}
-	if (!current_node && (current_index == idx))
+	new_node->n = n;
+
+	if ((int)idx == 0)
 	{
 		free(new_node);
-		return (add_dnodeint_end(h, n));
+		return (add_dnodeint(h, n));
 	}
 
-	free(new_node);
-	return (NULL);
+	while (tmp)
+	{
+		if (i == ((int)idx - 1))
+		{
+			if (!tmp->next)
+			{
+				free(new_node);
+				return (add_dnodeint_end(h, n));
+			}
+			new_node->next = tmp->next;
+			new_node->prev = tmp;
+			tmp->next->prev = new_node;
+			tmp->next = new_node;
+		}
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (((int)idx > i) || ((int)idx < 0))
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	return (new_node);
 }
