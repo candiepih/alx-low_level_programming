512cd5f7e943d378ec669011eae1150586f6ad62
candiepih <mutheeal.am@gmail.com>
2021-05-28 14:42:31 +0000

    formatted file in accordance to betty linter

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -183,7 +183,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
  */
 void shash_table_print(const shash_table_t *ht)
 {
-	int i;
 	shash_node_t *tmp;
 	int first_flag = 0;
 
@@ -211,7 +210,6 @@ void shash_table_print(const shash_table_t *ht)
  */
 void shash_table_print_rev(const shash_table_t *ht)
 {
-	int i;
 	shash_node_t *tmp;
 	int first_flag = 0;
 
@@ -260,7 +258,7 @@ void free_shash_node_t_list(shash_node_t *head)
  */
 void shash_table_delete(shash_table_t *ht)
 {
-	int i;
+	unsigned long int i;
 	shash_node_t *node;
 
 	if (ht == NULL)
