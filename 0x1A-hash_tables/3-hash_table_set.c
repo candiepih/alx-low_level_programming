52eddf0cb40cd178c1e5316a48ead25bb5cc2065
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-28 09:58:17 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(tmp->value);
 			tmp->value = strdup((char *)value);
 			if (tmp->value == NULL)
-				return (NULL);
+				return (0);
 			return (1);
 		}
 		tmp = tmp->next;
