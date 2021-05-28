ce8eeced92c5c4289f83e4f809d5f25a8733fb3c
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-28 09:40:39 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
-
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
@@ -28,6 +25,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
-
 	return (hash_table);
 }
