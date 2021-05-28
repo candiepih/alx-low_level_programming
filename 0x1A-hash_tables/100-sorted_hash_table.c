29c846bb98855fb93d3488a993266de72cfe0a97
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-28 17:01:37 +0300

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,1 +1,26 @@
 #include "hash_tables.h"
+
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *hash_table;
+	unsigned long int i;
+
+	hash_table = malloc(sizeof(shash_table_t));
+	if (!hash_table)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(shash_node_t *) * size);
+	if (!hash_table->array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	
+	hash_table->shead = NULL;
+	hash_table->stail = NULL;
+	return (hash_table);
+}
