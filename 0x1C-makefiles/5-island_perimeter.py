fb75c33e3fec537f10971bac21967255858c1890
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-02 20:16:35 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,27 +5,30 @@ island described in grid
 
 def determine_soroundings(array, y, x):
     """Determines whether soroundings has water or not
-    Assuming that land is 0 and water is 1 using xor and
-    land as mask we get value as either 1 or 0 to add to
-    perimeter
+    also if it's next or previous is at the ends
+    Args:
+        array (list): grid
+        y (int): current list index in `array`
+        x (int): current index in the list in `array`
     """
     mask = 1
     top = array[y - 1][x] ^ mask if y > 0 else 1
     bottom = array[y + 1][x] ^ mask if y < (len(array) - 1) else 1
     left = array[y][x - 1] ^ mask if x > 0 else 1
     right = array[y][x + 1] ^ mask if x < (len(array[y]) - 1) else 1
-
-    perimeter = top + bottom + right + left
-    return perimeter
+    p = top + bottom + right + left
+    return p
 
 def island_perimeter(grid):
     """Gets the perimeter determined by sorounding.
     all sides that do not have water we increase perimeter.
+    Args:
+        grid (list): list of lists
     """
     land = 1
-    p = 0
+    perimeter = 0
     for i, y in enumerate(grid):
         for j, x in enumerate(grid[i]):
             if x == land:
-                p += determine_soroundings(grid, i, j)
-    return p
+                perimeter += determine_soroundings(grid, i, j)
+    return perimeter
